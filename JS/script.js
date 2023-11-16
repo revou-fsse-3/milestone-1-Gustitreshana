78aa1be1f79d4af91551d5e04bf5a243a629d7f6
Gusti Treshana Herman <118178896+Gustitreshana@users.noreply.github.com>
2023-11-17 03:18:48 +0700

    Develop (#4)
    * add media query for 1024px - 1440px
    
    * add responsive tablet and laptop
    
    * add responsive for mobile
    
    * add class map line 454  and hamburger menu on navbar
    
    * Fix typo text
    
    * revise styling, layout, fix margin and padding, add border radius, change color box shadow. This is for dekstop version 1440p
    
    * move span oustide p in Pertanyaan Section
    
    * edit styling for responsive desktop 1024 - 1440
    
    * change shadow color on card kontak section
    
    * add some class on section
    
    * add responsive for mobile
    
    * add padding in class footer-container
    
    * test merge (last version, same code)
    
    * fix error due to excessive curly braces
    
    * add transition and animation on button
    
    * add id and onclick for function in JavaScript
    
    * add scroll behavior in body
    
    * add function for button
    
    * remove /assets to assets on fontface url

---
+++ JS/script.js
@@ -0,0 +1,51 @@
+// Tombol Tonton Video
+function linkVideo() {
+    window.location.href = "https://www.youtube.com/watch?v=TFQlz-Uh3Mk"
+}
+// Tombol Join Sekarang menuju form
+function clickJoin() {
+    window.location.href = "#joinMember"
+}
+// Tombol Pelajari Selengkapnya
+function linkWA() {
+    window.location.href = "https://wa.me/08777788889999"
+}
+// Tombol Tentang Kami (NAVBAR)
+function linkEmail() {
+    window.location.href = "mailto:Zenitabeauty@gmail.com"
+}
+// Tombol Beranda (NAVBAR)
+function refreshWeb() {
+    window.location.reload()
+}
+// Tombol Produk (NAVBAR)
+function sectionProduk() {
+    window.location.href = "#produkZB"
+}
+// Tombol Peluang Bisnis (NAVBAR)
+function sectionBisnis() {
+    window.location.href = "#bisnisZB"
+}
+
+// Function Tombol Submit Form (Kirim Pesan), Tombol tidak berfungsi jika input Nama & email belum diisi
+let form = document.getElementById("formZB")
+let input1 = document.getElementById("name")
+let input2 = document.getElementById("e-mail")
+let submitBtn = document.getElementById("btnForm")
+
+input1.addEventListener("input", checkForm)
+input2.addEventListener("input", checkForm)
+
+function checkForm() {
+    if (input1.value.trim() !== "" && input2.value.trim() !== "") {
+        submitBtn.removeAttribute("disabled")
+    } else {
+        submitBtn.setAttribute("disabled", "disabled")
+    }
+}
+// Alert dan reload page jika input nama dan email sudah diisi
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    alert("Data anda telah terkirim, kami akan segera menghubungi anda. Terimaksih!")
+    window.location.reload()
+})
